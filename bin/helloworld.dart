7642ce128b2488b857ce6bd7e1bad6cbbd978afa
Jim <jimjacquesson@gmail.com>
2025-05-15 16:04:47 +0200

    InitialCreate Dartwars

---
+++ bin/helloworld.dart
@@ -0,0 +1,6 @@
+//import 'package:helloworld/helloworld.dart' as helloworld;
+import 'package:helloworld/exo1.dart' as exo1;
+
+void main(List<String> arguments) {
+  exo1.run();
+}
---
+++ lib/helloworld.dart
@@ -0,0 +1,9 @@
+int calculate() {
+  return 6 * 7;
+}
+
+
+String hello() {
+  String firstname = "Jim";
+  return "hello $firstname";
+}
